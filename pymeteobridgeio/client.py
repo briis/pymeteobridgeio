b77c6eff4dd9bd97315c135a52077c83f1e28300
“Bjarne <bjarne@briis.com>
2020-05-25 17:41:42 +0200

    Rewritten module and minimized code

--- pymeteobridgeio/client.py
+++ pymeteobridgeio/client.py
@@ -6,16 +6,24 @@
 """
 
 import csv
-import aiohttp
+import asyncio
+from aiohttp import ClientSession, ClientTimeout
+from aiohttp.client_exceptions import ClientError
+from typing import Optional
+
 import logging
 from datetime import datetime
 
-
-class UnexpectedError(Exception):
-    """Other error."""
-
-    pass
-
+from pymeteobridgeio.const import (
+    DEFAULT_TIMEOUT,
+    UNIT_SYSTEM_METRIC,
+    UNIT_SYSTEM_IMPERIAL,
+)
+from pymeteobridgeio.errors import (
+    InvalidCredentials,
+    RequestError,
+    ResultError,
+)
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -25,178 +33,132 @@ class Meteobridge:
 
     def __init__(
         self,
-        session: aiohttp.ClientSession,
         Host: str,
         User: str,
         Pass: str,
-        unit_system: str,
+        unit_system: str = UNIT_SYSTEM_METRIC,
+        session: Optional[ClientSession] = None,
     ):
         self._host = Host
         self._user = User
         self._pass = Pass
-        self._unit_system = unit_system.lower()
-        self.sensor_data = {}
+        self._unit_system = unit_system
+        self._session: ClientSession = session
 
-        self.req = session
-
-    async def update(self) -> dict:
+    async def get_sensor_data(self) -> dict:
         """Updates the sensor data."""
-        await self._get_sensor_data()
-        return self.sensor_data
+        return await self._sensor_data()
 
     async def get_server_data(self) -> None:
         """Returns Meteobridge Server Data."""
+        return await self._meteobridge_server()
 
+    async def _meteobridge_server(self) -> None:
+        """Returns Meteobridge Server Specific Information."""
         data_template = "[mbsystem-mac:None];[mbsystem-swversion:0.0]-[mbsystem-buildnum:0];[mbsystem-platform:None];[mbsystem-station:None]"
         endpoint = f"http://{self._user}:{self._pass}@{self._host}/cgi-bin/template.cgi?template={data_template}"
+        data = await self.async_request("get", endpoint)
+        cr = csv.reader(data.splitlines(), delimiter=";")
+        rows = list(cr)
 
-        async with self.req.get(endpoint,) as response:
-            if response.status == 200:
-                content = await response.read()
-                decoded_content = content.decode("utf-8")
+        for values in rows:
+            item = {
+                "mac_address": values[0],
+                "swversion": values[1],
+                "platform_hw": values[2],
+                "station_hw": values[3],
+            }
+        return item
 
-                cr = csv.reader(decoded_content.splitlines(), delimiter=";")
-                rows = list(cr)
-                cnv = Conversion()
-                for values in rows:
-                    item = {
-                        "mac_address": values[0],
-                        "swversion": values[1],
-                        "platform_hw": values[2],
-                        "station_hw": values[3],
-                    }
-                return item
-
-            else:
-                raise UnexpectedError(
-                    f"Fetching Meteobridge data failed: {response.status} - Reason: {response.reason}"
-                )
-
-    async def _get_sensor_data(self) -> None:
+    async def _sensor_data(self) -> None:
         """Gets the sensor data from the Meteobridge Logger"""
 
         dataTemplate = "[DD]/[MM]/[YYYY];[hh]:[mm]:[ss];[th0temp-act:0];[thb0seapress-act:0];[th0hum-act:0];[wind0avgwind-act:0];[wind0dir-avg5.0:0];[rain0total-daysum:0];[rain0rate-act:0];[th0dew-act:0];[wind0chill-act:0];[wind0wind-max1:0];[th0lowbat-act.0:0];[thb0temp-act:0];[thb0hum-act.0:0];[th0temp-dmax:0];[th0temp-dmin:0];[wind0wind-act:0];[th0heatindex-act.1:0];[uv0index-act:0];[sol0rad-act:0];[th0temp-mmin.1:0];[th0temp-mmax.1:0];[th0temp-ymin.1:0];[th0temp-ymax.1:0];[wind0wind-mmax.1:0];[wind0wind-ymax.1:0];[rain0total-mmax.1:0];[rain0total-ymax.1:0];[rain0rate-mmax.1:0];[rain0rate-ymax.1:0];[forecast-text:]"
         endpoint = f"http://{self._user}:{self._pass}@{self._host}/cgi-bin/template.cgi?template={dataTemplate}"
 
-        async with self.req.get(endpoint,) as response:
-            if response.status == 200:
-                content = await response.read()
-                decoded_content = content.decode("utf-8")
+        data = await self.async_request("get", endpoint)
+        cr = csv.reader(data.splitlines(), delimiter=";")
+        rows = list(cr)
+        cnv = Conversion()
 
-                cr = csv.reader(decoded_content.splitlines(), delimiter=";")
-                rows = list(cr)
-                cnv = Conversion()
+        for values in rows:
+            self._outtemp = cnv.temperature(float(values[2]), self._unit_system)
+            self._heatindex = cnv.temperature(float(values[18]), self._unit_system)
+            self._windchill = cnv.temperature(float(values[10]), self._unit_system)
+            self._rainrate = cnv.rate(float(values[8]), self._unit_system)
+            sensor_item = {
+                "timestamp": datetime.strptime(f"{values[0]} {values[1]}", "%d/%m/%Y %H:%M:%S"),
+                "temperature": self._outtemp,
+                "pressure": cnv.pressure(float(values[3]), self._unit_system),
+                "humidity": values[4],
+                "windspeedavg": cnv.speed(float(values[5]), self._unit_system),
+                "windbearing": int(float(values[6])),
+                "winddirection": cnv.wind_direction(float(values[6])),
+                "raintoday": cnv.volume(float(values[7]), self._unit_system),
+                "rainrate": self._rainrate,
+                "dewpoint": cnv.temperature(float(values[9]), self._unit_system),
+                "windchill": self._windchill,
+                "windgust": cnv.speed(float(values[11]), self._unit_system),
+                "lowbattery": values[12],
+                "in_temperature": cnv.temperature(float(values[13]), self._unit_system),
+                "in_humidity": values[14],
+                "temphigh": cnv.temperature(float(values[15]), self._unit_system),
+                "templow": cnv.temperature(float(values[16]), self._unit_system),
+                "windspeed": cnv.speed(float(values[17]), self._unit_system),
+                "heatindex": self._heatindex,
+                "uvindex": float(values[19]),
+                "solarrad": float(values[20]),
+                "feels_like": cnv.feels_like(self._outtemp, self._heatindex, self._windchill, self._unit_system),
+                "tempmmin": cnv.temperature(float(values[21]), self._unit_system),
+                "tempmmax": cnv.temperature(float(values[22]), self._unit_system),
+                "tempymin": cnv.temperature(float(values[23]), self._unit_system),
+                "tempymax": cnv.temperature(float(values[24]), self._unit_system),
+                "windmmax": cnv.speed(float(values[25]), self._unit_system),
+                "windymax": cnv.speed(float(values[26]), self._unit_system),
+                "rainmmax": cnv.volume(float(values[27]), self._unit_system),
+                "rainymax": cnv.volume(float(values[28]), self._unit_system),
+                "rainratemmax": cnv.volume(float(values[29]), self._unit_system),
+                "rainrateymax": cnv.volume(float(values[30]), self._unit_system),
+                "forecast": values[31],
+                "isfreezing": True if float(self._outtemp) < 0 else False,
+                "israining": True if float(self._rainrate) > 0 else False,
+                "islowbat": True if float(values[12]) > 0 else False
+            }
 
-                for values in rows:
-                    self._timestamp = datetime.strptime(
-                        values[0] + " " + values[1], "%d/%m/%Y %H:%M:%S"
-                    )
+        return sensor_item
 
-                    self._outtemp = cnv.temperature(float(values[2]), self._unit_system)
-                    self._press = cnv.pressure(float(values[3]), self._unit_system)
-                    self._outhum = values[4]
-                    self._windspeedavg = cnv.speed(float(values[5]), self._unit_system)
-                    self._windbearing = int(float(values[6]))
-                    self._winddir = cnv.wind_direction(float(values[6]))
-                    self._raintoday = cnv.volume(float(values[7]), self._unit_system)
-                    self._rainrate = cnv.rate(float(values[8]), self._unit_system)
-                    self._outdew = cnv.temperature(float(values[9]), self._unit_system)
-                    self._windchill = cnv.temperature(
-                        float(values[10]), self._unit_system
-                    )
-                    self._windgust = cnv.speed(float(values[11]), self._unit_system)
-                    self._lowbat = values[12]
-                    self._intemp = cnv.temperature(float(values[13]), self._unit_system)
-                    self._inhum = values[14]
-                    self._temphigh = cnv.temperature(
-                        float(values[15]), self._unit_system
-                    )
-                    self._templow = cnv.temperature(
-                        float(values[16]), self._unit_system
-                    )
-                    self._windspeed = cnv.speed(float(values[17]), self._unit_system)
-                    self._heatindex = cnv.temperature(
-                        float(values[18]), self._unit_system
-                    )
-                    self._uvindex = float(values[19])
-                    self._solarrad = float(values[20])
-                    self._feels_like = cnv.feels_like(
-                        self._outtemp,
-                        self._heatindex,
-                        self._windchill,
-                        self._unit_system,
-                    )
-                    self._tempmmin = cnv.temperature(
-                        float(values[21]), self._unit_system
-                    )
-                    self._tempmmax = cnv.temperature(
-                        float(values[22]), self._unit_system
-                    )
-                    self._tempymin = cnv.temperature(
-                        float(values[23]), self._unit_system
-                    )
-                    self._tempymax = cnv.temperature(
-                        float(values[24]), self._unit_system
-                    )
-                    self._windmmax = cnv.speed(float(values[25]), self._unit_system)
-                    self._windymax = cnv.speed(float(values[26]), self._unit_system)
-                    self._rainmmax = cnv.volume(float(values[27]), self._unit_system)
-                    self._rainymax = cnv.volume(float(values[28]), self._unit_system)
-                    self._rainratemmax = cnv.volume(
-                        float(values[29]), self._unit_system
-                    )
-                    self._rainrateymax = cnv.volume(
-                        float(values[30]), self._unit_system
-                    )
-                    self._fc = values[31]
+    async def async_request(self, method: str, endpoint: str) -> dict:
+        """Make a request against the SmartWeather API."""
 
-                    self._isfreezing = True if float(self._outtemp) < 0 else False
-                    self._israining = True if float(self._rainrate) > 0 else False
-                    self._islowbat = True if float(self._lowbat) > 0 else False
+        use_running_session = self._session and not self._session.closed
 
-                item = {
-                    "in_temperature": self._intemp,
-                    "in_humidity": self._inhum,
-                    "temperature": self._outtemp,
-                    "temphigh": self._temphigh,
-                    "templow": self._templow,
-                    "humidity": self._outhum,
-                    "dewpoint": self._outdew,
-                    "windbearing": self._windbearing,
-                    "winddirection": self._winddir,
-                    "windspeedavg": self._windspeedavg,
-                    "windspeed": self._windspeed,
-                    "windgust": self._windgust,
-                    "windchill": self._windchill,
-                    "heatindex": self._heatindex,
-                    "feels_like": self._feels_like,
-                    "pressure": self._press,
-                    "rainrate": self._rainrate,
-                    "raintoday": self._raintoday,
-                    "uvindex": self._uvindex,
-                    "solarrad": self._solarrad,
-                    "lowbattery": self._islowbat,
-                    "raining": self._israining,
-                    "freezing": self._isfreezing,
-                    "forecast": self._fc,
-                    "time": self._timestamp.strftime("%d-%m-%Y %H:%M:%S"),
-                    "temp_mmin": self._tempmmin,
-                    "temp_mmax": self._tempmmax,
-                    "temp_ymin": self._tempymin,
-                    "temp_ymax": self._tempymax,
-                    "windspeed_mmax": self._windmmax,
-                    "windspeed_ymax": self._windymax,
-                    "rain_mmax": self._rainmmax,
-                    "rain_ymax": self._rainymax,
-                    "rainrate_mmax": self._rainratemmax,
-                    "rainrate_ymax": self._rainrateymax,
-                }
-                self.sensor_data.update(item)
+        if use_running_session:
+            session = self._session
+        else:
+            session = ClientSession(timeout=ClientTimeout(total=DEFAULT_TIMEOUT))
+
+        try:
+            async with session.request(
+                method, endpoint
+            ) as resp:
+                resp.raise_for_status()
+                data = await resp.read()
+                decoded_content = data.decode("utf-8")
+                return decoded_content
+        except asyncio.TimeoutError:
+            raise RequestError("Request to endpoint timed out: {endpoint}")
+        except ClientError as err:
+            if err.message == "Unauthorized":
+                raise InvalidCredentials("Your Username/Password combination is not correct")
+            elif err.message == "Not Found":
+                raise ResultError("The Meteobridge cannot not be found on this IP Address")
             else:
-                raise UnexpectedError(
-                    f"Fetching Meteobridge data failed: {response.status} - Reason: {response.reason}"
-                )
-
+                raise RequestError(
+                    f"Error requesting data from {endpoint}: {err}"
+                ) from None
+        finally:
+            if not use_running_session:
+                await session.close()
 
 class Conversion:
 
