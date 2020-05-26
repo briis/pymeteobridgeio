c96769d4c49a1819dced6b84d2ce6b8ac855bc16
“Bjarne <bjarne@briis.com>
2020-05-26 07:18:00 +0200

    Moved helper function to separate class

--- pymeteobridgeio/client.py
+++ pymeteobridgeio/client.py
@@ -24,6 +24,7 @@ from pymeteobridgeio.errors import (
     RequestError,
     ResultError,
 )
+from pymeteobridgeio.functions import Conversion
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -160,104 +161,3 @@ class Meteobridge:
             if not use_running_session:
                 await session.close()
 
-class Conversion:
-
-    """
-    Conversion Class to convert between different units.
-    WeatherFlow always delivers values in the following formats:
-    Temperature: C
-    Wind Speed: m/s
-    Wind Direction: Degrees
-    Pressure: mb
-    Distance: km
-    """
-
-    def temperature(self, value, unit):
-        if unit == "imperial":
-            # Return value F
-            return round((value * 9 / 5) + 32, 1)
-        else:
-            # Return value C
-            return round(value, 1)
-
-    def volume(self, value, unit):
-        if unit == "imperial":
-            # Return value in
-            return round(value * 0.0393700787, 2)
-        else:
-            # Return value mm
-            return round(value, 1)
-
-    def rate(self, value, unit):
-        if unit == "imperial":
-            # Return value in
-            return round(value * 0.0393700787, 2)
-        else:
-            # Return value mm
-            return round(value, 2)
-
-    def pressure(self, value, unit):
-        if unit == "imperial":
-            # Return value inHg
-            return round(value * 0.0295299801647, 3)
-        else:
-            # Return value mb
-            return round(value, 1)
-
-    def speed(self, value, unit):
-        if unit == "imperial":
-            # Return value in mi/h
-            return round(value * 2.2369362921, 1)
-        elif unit == "uk":
-            # Return value in km/h
-            return round(value * 3.6, 1)
-        else:
-            # Return value in m/s
-            return round(value, 1)
-
-    def distance(self, value, unit):
-        if unit == "imperial":
-            # Return value in mi
-            return round(value * 0.621371192, 1)
-        else:
-            # Return value in km
-            return round(value, 0)
-
-    def feels_like(self, temp, heatindex, windchill, unit):
-        """ Return Feels Like Temp."""
-        if unit == "imperial":
-            high_temp = 80
-            low_temp = 50
-        else:
-            high_temp = 26.666666667
-            low_temp = 10
-
-        if float(temp) > high_temp:
-            return float(heatindex)
-        elif float(temp) < low_temp:
-            return float(windchill)
-        else:
-            return temp
-
-    def wind_direction(self, bearing):
-        direction_array = [
-            "N",
-            "NNE",
-            "NE",
-            "ENE",
-            "E",
-            "ESE",
-            "SE",
-            "SSE",
-            "S",
-            "SSW",
-            "SW",
-            "WSW",
-            "W",
-            "WNW",
-            "NW",
-            "NNW",
-            "N",
-        ]
-        direction = direction_array[int((bearing + 11.25) / 22.5)]
-        return direction
