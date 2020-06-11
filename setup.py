ec34842334c53c165a1d57bd06d851a137a62b6a
“Bjarne <bjarne@briis.com>
2020-06-11 10:56:13 +0200

    Bumped version to 0.17

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'pymeteobridgeio',
   packages = ['pymeteobridgeio'],
-  version = '0.16',
+  version = '0.17',
   license='MIT',
   description = 'Python Wrapper for Meteobridge Data Logger', 
   author = 'Bjarne Riis',
