35d8a94c263110d1f5c4348d4fb46528be03a39f
Bjarne Riis <bjarne@briis.com>
2020-11-02 09:31:21 +0100

    Bumped to 0.21.5

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     "": ["*.md"],
     "pymeteobridgeio": ["translations/*.json"],
   },
-  version = '0.21.4',
+  version = '0.21.5',
   license='MIT',
   description = 'Python Wrapper for Meteobridge Data Logger', 
   author = 'Bjarne Riis',
