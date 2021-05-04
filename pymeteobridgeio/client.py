5b19794cdee9328fb0415c458b6380300b6d8fa8
Bjarne Riis <bjarne@briis.com>
2021-05-04 07:38:46 +0200

    Added absolute pressure

--- pymeteobridgeio/client.py
+++ pymeteobridgeio/client.py
@@ -105,7 +105,7 @@ class Meteobridge:
     async def _sensor_data(self) -> None:
         """Gets the sensor data from the Meteobridge Logger"""
 
-        dataTemplate = "[DD]/[MM]/[YYYY];[hh]:[mm]:[ss];[th0temp-act:0];[thb0seapress-act:0];[th0hum-act:0];[wind0avgwind-act:0];[wind0dir-avg5.0:0];[rain0total-daysum:0];[rain0rate-act:0];[th0dew-act:0];[wind0chill-act:0];[wind0wind-max1:0];[th0lowbat-act.0:0];[thb0temp-act:0];[thb0hum-act.0:0];[th0temp-dmax:0];[th0temp-dmin:0];[wind0wind-act:0];[th0heatindex-act.1:0];[uv0index-act:0];[sol0rad-act:0];[th0temp-mmin.1:0];[th0temp-mmax.1:0];[th0temp-ymin.1:0];[th0temp-ymax.1:0];[wind0wind-mmax.1:0];[wind0wind-ymax.1:0];[rain0total-mmax.1:0];[rain0total-ymax.1:0];[rain0rate-mmax.1:0];[rain0rate-ymax.1:0];[lgt0total-act.0:0];[lgt0energy-act.0:0];[lgt0dist-act.0:0];[air0pm-act.0:0];[wind0wind-act=bft.0:0];[th1temp-act:0];[th1hum-act:0];[th1heatindex-act.1:0];[th2temp-act:0];[th2hum-act:0];[th2heatindex-act.1:0];[th0temp-delta10:0];[thb0seapress-delta10:0];[forecast-text:]"
+        dataTemplate = "[DD]/[MM]/[YYYY];[hh]:[mm]:[ss];[th0temp-act:0];[thb0seapress-act:0];[th0hum-act:0];[wind0avgwind-act:0];[wind0dir-avg5.0:0];[rain0total-daysum:0];[rain0rate-act:0];[th0dew-act:0];[wind0chill-act:0];[wind0wind-max1:0];[th0lowbat-act.0:0];[thb0temp-act:0];[thb0hum-act.0:0];[th0temp-dmax:0];[th0temp-dmin:0];[wind0wind-act:0];[th0heatindex-act.1:0];[uv0index-act:0];[sol0rad-act:0];[th0temp-mmin.1:0];[th0temp-mmax.1:0];[th0temp-ymin.1:0];[th0temp-ymax.1:0];[wind0wind-mmax.1:0];[wind0wind-ymax.1:0];[rain0total-mmax.1:0];[rain0total-ymax.1:0];[rain0rate-mmax.1:0];[rain0rate-ymax.1:0];[lgt0total-act.0:0];[lgt0energy-act.0:0];[lgt0dist-act.0:0];[air0pm-act.0:0];[wind0wind-act=bft.0:0];[th1temp-act:0];[th1hum-act:0];[th1heatindex-act.1:0];[th2temp-act:0];[th2hum-act:0];[th2heatindex-act.1:0];[th0temp-delta10:0];[thb0seapress-delta10:0];[thb0press-act:0];[forecast-text:]"
         endpoint = f"http://{self._user}:{self._pass}@{self._host}/cgi-bin/template.cgi?template={dataTemplate}"
 
         data = await self.async_request("get", endpoint)
@@ -433,8 +433,16 @@ class Meteobridge:
                     "icon": "trending-down",
                     "unit": await cnv.trend_text(values[43],self._language),
                     },
+                "absolute_pressure": {
+                    "value": await cnv.pressure(float(values[44]), self._unit_pressure),
+                    "name": "Absolute Pressure",
+                    "type": DEVICE_TYPE_SENSOR,
+                    "device_class": DEVICE_CLASS_PRESSURE,
+                    "icon": "gauge",
+                    "unit": self._unit_pressure,
+                    },
                 "forecast": {
-                    "value": values[44],
+                    "value": values[45],
                     "name": "Station forecast",
                     "type": DEVICE_TYPE_SENSOR,
                     "device_class": DEVICE_CLASS_NONE,
